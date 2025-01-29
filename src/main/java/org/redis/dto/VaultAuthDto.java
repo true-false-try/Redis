3e7576f8ee702a7d646f604dfdf533b94fc8e30d
true-false-try <6mateo6ch@gmail.com>
2025-01-29 20:16:10 +0200

    [APP-4] - app ollama caller

--- src/main/java/org/redis/dto/VaultAuthDto.java
+++ src/main/java/org/redis/dto/VaultAuthDto.java
@@ -3,12 +3,13 @@ package org.redis.dto;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
 import java.util.Map;
 
 @Data
 @RequiredArgsConstructor
 public class VaultAuthDto {
-    private Map<String, String> unseals;
+    private List<String> unseals;
     private String rootToken;
 
 }
--- src/main/java/org/redis/model/VaultAuthModel.java
+++ src/main/java/org/redis/model/VaultAuthModel.java
@@ -3,11 +3,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sun.istack.NotNull;
 import lombok.Data;
 
+import java.util.List;
 import java.util.Map;
 
 @Data
 public class VaultAuthModel {
-    private Map<String, String> unseals;
+    private List<String> unseals;
     @JsonProperty("root-token")
     private String rootToken;
 }
